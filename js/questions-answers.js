564befe561f636f894c80d33cd242e5e1f0d71d6
louisvillemsd <33227034+louisvillemsd@users.noreply.github.com>
2017-10-30 10:44:12 -0400

    Add files via upload

---
+++ js/questions-answers.js
@@ -0,0 +1,74 @@
+var data = [
+  ["Top 6 Metro 311 Calls", [
+    'Junk Week', 29,
+    'High Grass & Weeds', 23,
+    'Trash on Private Property', 22, 
+    'Damaged Garbage Carts', 21, 
+    'Abandoned Vehicles ', 8,
+    'Street Pothole', 7
+  ]],
+  ["Metro Departments With The Most Employees", [
+    'LMPD', 25,
+    'Corrections', 22,
+    'Parks', 19,
+    'Fire & Rescue', 15,
+    'Public Works', 11,
+    'Library', 10
+  ]],
+  ["Top 6 things you hear at Louie Stat", [
+    'PDCA', 25,
+    'Overtime reduction', 22,
+    'Safety Tip', 19,
+    'TIME!!!', 15,
+    'Best Practices', 11,
+    'Fascinating...', 10
+  ]],
+  ["Metro Parks Designed by Olmsted", [
+    'Cherokee', 25,
+    'Shawnee', 22,
+    'Iroquois', 19,
+    'Seneca', 15,
+    'Chickasaw', 11,
+    'Central', 5
+  ]],
+  ["Louisville Landmarks that are no longer but still referenced", [
+    'Old Sears', 25,
+    'Cinemas (Showcase or Drive In)', 22,
+    'Bashford Manor Mall', 21,
+    'Bacons', 20,
+    'Tumbleweed on the river', 19,
+    'Water fountain on the river', 15
+  ]],
+  ["Downtown Bourbon Experiences", [
+    'Evan Williams', 25,
+    'Angels Envy', 22,
+    'Jim Beam ', 19,
+    'Kentucky Peerless', 15,
+    'Mitchers', 11,
+    'Blank', 1
+  ]],
+  ["Most Recent Triple Crown Winners", [
+    'American Pharoah', 25,
+    'Secretariat', 22,
+    'Affirmed', 19,
+    'Seattle Slew', 15,
+    'War Admiral', 11,
+    'Citation', 1
+  ]],
+  ["Neighborhoods in West Louisville", [
+    'Shawnee', 25,
+    'Russell', 22,
+    'Portland', 19,
+    'California', 15,
+    'Chickasaw', 11,
+    'Algonquin', 0
+  ]],
+  ["Famous Foods / Drinks Associated with Louisville/Kentucky", [
+    'Mint Julep/Bourbon', 25,
+    'Hot Brown', 22,
+    'Benedictine', 19,
+    'Derby Pie', 15,
+    'Cheeseburger', 11,
+    'Burgoo', 0
+  ]]
+];
